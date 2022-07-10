02058551b5cd66f2df7a897185bf36512239e4f4
Becca Doan <rebecca.doan@yahoo.com>
2022-07-10 15:13:54 -0500

    finished prompt and gathering all necessary team data in one object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,18 +1,18 @@
 const Employee = require('../lib/Employee');
 
 test('creates an employee object', () => {
-    const employee = new Employee('Becca',95,'rebecca.doan@yahoo.com');
+    const employee = new Employee('Becca','95','rebecca.doan@yahoo.com');
 
     expect(employee.name).toBe('Becca');
-    expect(employee.id).toBe(95);
+    expect(employee.id).toBe('95');
     expect(employee.email).toBe('rebecca.doan@yahoo.com');
 });
 
 test('tests employee methods', () => {
-    const employee = new Employee('Becca',95,'rebecca.doan@yahoo.com');
+    const employee = new Employee('Becca','95','rebecca.doan@yahoo.com');
 
     expect(employee.getName()).toBe('Becca');
-    expect(employee.getId()).toBe(95);
+    expect(employee.getId()).toBe('95');
     expect(employee.getEmail()).toBe('rebecca.doan@yahoo.com');
     expect(employee.getRole()).toBe('Employee');
 });
